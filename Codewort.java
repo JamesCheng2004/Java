b7ae31bcd724b2f61d0a8e8679d70681c99a26a9
JamesCheng2004 <69053004+JamesCheng2004@users.noreply.github.com>
2023-10-29 22:44:56 +0100

    Update Codewort.java

---
+++ Codewort.java
@@ -0,0 +1,9 @@
+public class Codewort {
+    public static void main(String[] args) {
+        String codewort = "nicht abgegeben";
+        String githubBenutzername = "JamesCheng2004";
+
+        System.out.println("Codewort: " + codewort);
+        System.out.println("GitHub-Benutzername: " + githubBenutzername);
+    }
+}
